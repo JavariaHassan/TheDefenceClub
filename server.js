915f620a271917cd771ca65b337dc80289bf5d82
HamzaJawed <m.hamza@live.com>
2019-04-14 16:39:05 +0500

    pass data from one screen to another

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
-// var port = process.env.PORT || 3000;
+// const port = 3000
+var port = process.env.PORT || 3000;
 bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
