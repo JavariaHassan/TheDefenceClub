e4fb17a0696695b4521cd80492dbf8ebc6d1510d
Javaria Hassan <javariahassan@Javarias-MacBook-Pro.local>
2019-04-17 00:28:19 +0500

    cute animation

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Keyboard, Platform, Dimensions, StyleSheet, Text, TextInput, View, Image, ImageBackground, TouchableOpacity, Alert} from 'react-native';
+import {ActivityIndicator, Animated, Keyboard, Platform, Dimensions, StyleSheet, Text, TextInput, View, Image, ImageBackground, TouchableOpacity, Alert} from 'react-native';
 
 var width = Dimensions.get('window').width;
 var height = Dimensions.get('window').height;
@@ -10,15 +10,59 @@ export default class LoginScreen extends Component {
         super(props);
         this.state = { Username: 'username',
                        Password: 'password',
-                       invalid: 0 };
+                       invalid: 0,
+                       length:  new Animated.Value(0.5*width),
+                       borderRadius:  new Animated.Value(10),
+                       load: 0,
+                       eye: '',
+                       showPassword: true,
+                     };
     }
 
     static navigationOptions = {
         header: null
     }
+
+    signinAnimation = () => {
+        this.setState({load: 1})
+        if (1) {
+            Animated.timing(                  
+            this.state.length,            
+                {
+                    toValue: 0.09*width,             
+                    duration: 300,         
+                },
+            ).start();
+        }
+        this.setState({borderRadius: 0.09*width})
+    }
+
+    signoutAnimation = () => {
+        this.setState({load: 0})
+        if (1) {
+            Animated.timing(                  
+            this.state.length,            
+                {
+                    toValue: 0.5*width,             
+                    duration: 300,         
+                },
+            ).start();
+        }
+        this.setState({borderRadius: 10})
+    }
+
+    eye = () => {
+    	this.setState({ showPassword: !this.state.showPassword });
+    	if (this.state.eye == '') {
+    		this.setState({eye: ''})
+    	} else {
+    		this.setState({eye: ''})
+    	}
+    }
     
     onPress = () => {
         Keyboard.dismiss();
+        this.signinAnimation()
 
         const data = {
             username : this.state.Username,
@@ -49,42 +93,66 @@ export default class LoginScreen extends Component {
                     this.setState({invalid: 0})
                 }else{
                     this.setState({invalid: 1})
+                    this.signoutAnimation()
                 }
                 })
         }
-        Authenticate(data).catch(alert => {});
+        Authenticate(data).catch(alert => {this.signoutAnimation()});
     }
     
     render() {
+
+        const AnimatedTouchableOpacity = Animated.createAnimatedComponent(TouchableOpacity)
+
         return (
             <ImageBackground source={require('./BG_1.png')} style={styles.container}>
                 <View style={styles.backbox}>
                     <Image source={require('./logo.png')} style={styles.logo}/>
                 
                     <Text style={styles.welcome}>Welcome</Text>
-                
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Username"
-                        onChangeText={(Username) => this.setState({Username})}
-                    />
-                
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Password"
-                        onChangeText={(Password) => this.setState({Password})}
-                    />
+                	
+                	<View style={styles.input}>
+	                	<View style={styles.input1}>
+	                		<Text style={styles.icon}>  </Text>
+		                    <TextInput
+		                        style={styles.input2}
+		                        autoCorrect={false}
+		                        placeholder="Username"
+		                        placeholderTextColor="#5E5E5E"
+		                        onChangeText={(Username) => this.setState({Username})}
+		                    />
+	                    </View>
+                    </View>
+                	
+                	<View style={styles.input}>
+	                	<View style={styles.input1}>
+	                		<TouchableOpacity onPress={this.eye}>
+	                			<Text style={styles.icon}> {this.state.eye} </Text>
+	                		</TouchableOpacity>
+		                    <TextInput
+		                    	secureTextEntry={this.state.showPassword}
+		                    	autoCorrect={false}
+		                        style={styles.input2}
+		                        placeholder="Password"
+		                        placeholderTextColor="#5E5E5E"
+		                        onChangeText={(Password) => this.setState({Password})}
+		                    />
+			            </View>
+                    </View>
                 
                     {this.state.invalid ?
                         <Text style={styles.invalid}>Invalid username or password</Text>
-                        : null}
+                        : <Text style={[styles.invalid, {color: 'rgba(255,255, 255,0)'}]}>Invalid username or password</Text>}
                 
-                    <TouchableOpacity
-                        style={styles.signinbutton}
+                    <AnimatedTouchableOpacity
+                        style={[styles.signinbutton, {width: this.state.length, borderRadius: this.state.borderRadius}]}
                         onPress={this.onPress}
                     >
-                        <Text style={{color: 'white', fontSize: 0.04*width}}> SIGN IN </Text>
-                    </TouchableOpacity>
+                        {this.state.load ?
+                            <ActivityIndicator size="small" color="white" />
+                            : <Text style={{color: 'white', fontSize: 0.038*width, fontFamily: 'Calibri', fontWeight: 'bold'}}> SIGN IN </Text>}
+                        
+                    </AnimatedTouchableOpacity>
     
                 </View>
             </ImageBackground>
@@ -99,48 +167,68 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     backbox: {
-        height: 0.70*height,
+        height: 1.2*width,
         width: 0.70*width,
-        backgroundColor: 'rgba(255,255, 255,0.75)',
+        backgroundColor: 'rgba(255,255, 255,0.8)',
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 0.1*width,
+        borderRadius: 20,
     },
     logo: {
-        height: 0.25*width,
-        width: 0.20*width,
-        position: 'absolute',
-        top: 0.05*height,
+        height: 0.3*width,
+        width: 0.25*width,
+        marginBottom: 0.08*width,
     },
     welcome: {
-        fontSize: 0.05*width,
+        fontFamily: 'Cookie',
+        fontSize: 0.095*width,
         color: '#23186A',
-        paddingBottom: 0.025*height,
+        marginBottom: 0.07*width,
     },
     input: {
-        color: '#767171',
-        borderBottomColor: '#767171',
-        borderBottomWidth: 1,
-        width: 0.50*width,
-        fontSize: 0.035*width,
-        paddingBottom: 0,
-        marginBottom: 0.01*height,
+        backgroundColor: "white",
+        width: 0.5*width,
+        height: 0.09*width,
+        paddingHorizontal: 10,
+        borderRadius: 10,
+        marginTop: 0.03*width,
+        borderColor: "#D9D8D9",
+        borderWidth: 1,
+    },
+    input1: {
+        flex: 1,
+        flexDirection: 'row',
+    },
+    input2: {
+        height: 0.09*width,
+    	width: 0.35*width,
+        fontFamily: "Calibri",
+        color: '#5E5E5E',
+        fontSize: 0.038*width,
+    },
+    icon: {
+        height: 0.09*width,
+    	width: 0.07*width,
+    	fontFamily: "Font Awesome 5 Free",
+        color: '#5E5E5E',
+        fontSize: 0.03*width,
+        lineHeight: 0.075*width,
     },
     signinbutton: {
-        backgroundColor: '#23186A',
-        marginTop: 0.05*height,
-        width: 0.35*width,
-        height: 0.07*height,
-        justifyContent: 'center',
+        marginTop: 0.07*width,
+        backgroundColor: "#23186A",
+        color: 'white',
+        width: 0.5*width,
+        height: 0.09*width,
         alignItems: 'center',
-        borderRadius: width,
-        position: 'absolute',
-        bottom: 0.1*height,
+        justifyContent: 'center',
     },
     invalid: {
         color: 'red',
+        marginTop: 5,
         fontSize: 0.03*width,
         textAlign: 'left',
-        width: 0.50*width,
+        width: 0.5*width,
+        marginLeft: 10,
     },
 });
